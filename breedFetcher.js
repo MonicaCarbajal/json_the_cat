b2785bf82089e07bb8e8c8d7a3607dd00bac9c05
Monica Carbajal <monica.carbajal2@gmail.com>
2023-08-05 19:45:44 -0600

    adding mocha tested files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,22 @@
 const request = require('request');
 
-const fetchBreedData = (breedName) => {
+const fetchBreedDescription = function(breedName, callback) {
   const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
   request(apiUrl, (error, response, body) => {
     if (error) {
-      console.error('Error fetching data:', error);
+      callback(error, null);
     } else {
       const data = JSON.parse(body);
-
       if (Array.isArray(data) && data.length > 0) {
         const firstEntry = data[0];
         const description = firstEntry.description;
-        console.log('Description:', description);
+        callback(null, description);
       } else {
-        console.log('No data found for the breed:', breedName);
+        callback(`No data found for the breed: ${breedName}`, null);
       }
     }
   });
 };
 
-// accept command-line arguments
-const args = process.argv.slice(2);
-const breedName = args[0];
-
-fetchBreedData(breedName);
+module.exports = { fetchBreedDescription };
